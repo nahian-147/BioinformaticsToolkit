81cf1c7bec409f92e9818fb2a990b2eecee1a4da
kaziNAHIAN <provat44.knhp@gmail.com>
2022-12-04 15:59:44 +0600

    home added

--- bioinformatics_toolkit/urls.py
+++ bioinformatics_toolkit/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('basicClassesAndForms.urls')),
     path('genome_manipulation/', include('genomeManipulation.urls')),
     path('gene_prediction/', include('genePrediction.urls')),
 ]
